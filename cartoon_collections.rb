a763ba5d8baafae37ed0e4b295f2dea351ac78aa
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 12:40:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,12 @@ def roll_call_dwarves(cartoons)
       dwarves = %w[Dopey Grumpy Bashful]
     end
 
-def summon_captain_planet# code an argument here
-  
-end
+def summon_captain_planet
+   capitalizes each element and adds an exclamation mark (test 1)
+      fruits = %w[apple banana orange]
+      result = summon_captain_planet(fruits)
+      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
+    end
 
 def long_planeteer_calls# code an argument here
   # Your code here
