a337840795936c32f3df4e20e84fa5468ebb7007
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 12:42:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,11 @@ def summon_captain_planet
       ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
     end
 
-def long_planeteer_calls# code an argument here
-  
+def long_planeteer_calls(calls_long)
+  #returns true if any calls are longer than 4 characters
+      calls_long = ["axe", "earth", "wind", "fire"]
+      expect(long_planeteer_calls(calls_long))=(true)
+    end
 end
 
 def find_the_cheese# code an argument here
