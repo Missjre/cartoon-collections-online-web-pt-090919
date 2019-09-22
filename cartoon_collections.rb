b429ab50c683c46d3fce66acddbfbc9c7ea55f77
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 12:45:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(cartoons)
+ puts prints out the 7 dwarfs in a numbered list
+      dwarves = %w[Dopey Grumpy Bashful]
+    end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def summon_captain_planet
+      fruits = %w[apple banana orange]
+      result = summon_captain_planet(fruits)
+      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
+    end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def long_planeteer_calls(calls_long)
+      calls_long = ["axe", "earth", "wind", "fire"]
+      expect(long_planeteer_calls(calls_long))=(true)
+    end
+
+def find_the_cheese(cheese_types)
+      cheddar_cheese = %w[banana cheddar sock]
+      cheese_types(find_the_cheese(cheddar_cheese))=(cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
